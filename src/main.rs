82a4664a30ed3b608c2507a31b5fbc5e0286c8a6
jan <defPelly@proton.me>
2025-07-12 12:18:09 +0200

    Bunch of stuff - Add remaining arithmetic operations - support int and float as values - Value enum + arithmetic operator implementations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use stacked::svm::*;
 
 fn main() {
-    let program: Vec<Inst> = vec![Inst::Push(1), Inst::Print, Inst::Jump(0), Inst::Halt];
+    let program: Vec<Inst> = vec![
+        Inst::Push(1.into()),
+        Inst::Push(10.into()),
+        Inst::Add,
+        Inst::Print,
+        Inst::Halt,
+    ];
     let mut svm = Svm::from_program(program);
     svm.run();
 }
--- src/svm.rs
+++ src/svm.rs
@@ -1,12 +1,110 @@
-use std::process::exit;
+use core::ops::{Add, Div, Mul, Sub};
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::{array, process::exit};
 
 const STACK_SIZE: usize = 10;
 
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Value {
+    Int(i64),
+    Float(f64),
+}
+
+impl From<i64> for Value {
+    fn from(val: i64) -> Self {
+        Self::Int(val)
+    }
+}
+
+impl From<f64> for Value {
+    fn from(val: f64) -> Self {
+        Self::Float(val)
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            &Value::Int(val) => {
+                write!(f, "{:.4}", val)?;
+            }
+            &Value::Float(val) => {
+                write!(f, "{}", val)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Add for Value {
+    type Output = Value;
+    fn add(self, rhs: Self) -> Self::Output {
+        match self {
+            Value::Int(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val + rhs_val).into(),
+                Value::Float(rhs_val) => (lhs_val as f64 + rhs_val).into(),
+            },
+            Value::Float(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val + rhs_val as f64).into(),
+                Value::Float(rhs_val) => (lhs_val + rhs_val).into(),
+            },
+        }
+    }
+}
+
+impl Sub for Value {
+    type Output = Value;
+    fn sub(self, rhs: Self) -> Self::Output {
+        match self {
+            Value::Int(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val - rhs_val).into(),
+                Value::Float(rhs_val) => (lhs_val as f64 - rhs_val).into(),
+            },
+            Value::Float(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val - rhs_val as f64).into(),
+                Value::Float(rhs_val) => (lhs_val - rhs_val).into(),
+            },
+        }
+    }
+}
+impl Mul for Value {
+    type Output = Value;
+    fn mul(self, rhs: Self) -> Self::Output {
+        match self {
+            Value::Int(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val * rhs_val).into(),
+                Value::Float(rhs_val) => (lhs_val as f64 * rhs_val).into(),
+            },
+            Value::Float(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val * rhs_val as f64).into(),
+                Value::Float(rhs_val) => (lhs_val * rhs_val).into(),
+            },
+        }
+    }
+}
+impl Div for Value {
+    type Output = Value;
+    fn div(self, rhs: Self) -> Self::Output {
+        match self {
+            Value::Int(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val / rhs_val).into(),
+                Value::Float(rhs_val) => (lhs_val as f64 / rhs_val).into(),
+            },
+            Value::Float(lhs_val) => match rhs {
+                Value::Int(rhs_val) => (lhs_val / rhs_val as f64).into(),
+                Value::Float(rhs_val) => (lhs_val / rhs_val).into(),
+            },
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum Inst {
-    Push(i64),
+    Push(Value),
     Add,
     Sub,
+    Mul,
+    Div,
     Print, // prints the stack
     Jump(usize),
     Halt,
@@ -16,7 +114,7 @@ pub enum Inst {
 pub struct Svm {
     pub ip: usize,
     pub sp: usize,
-    pub stack: [i64; STACK_SIZE],
+    pub stack: [Value; STACK_SIZE],
     pub insts: Vec<Inst>,
 }
 
@@ -25,7 +123,7 @@ impl Svm {
         Svm {
             ip: 0,
             sp: 0,
-            stack: [0; STACK_SIZE],
+            stack: array::from_fn(|_| 0.into()),
             insts: program,
         }
     }
@@ -53,6 +151,18 @@ impl Svm {
                     Inst::Sub => {
                         self.check_stack_underflow(2);
                         self.stack[self.sp - 2] = self.stack[self.sp - 2] - self.stack[self.sp - 1];
+                        self.sp -= 1;
+                        self.ip += 1;
+                    }
+                    Inst::Mul => {
+                        self.check_stack_underflow(2);
+                        self.stack[self.sp - 2] = self.stack[self.sp - 2] * self.stack[self.sp - 1];
+                        self.sp -= 1;
+                        self.ip += 1;
+                    }
+                    Inst::Div => {
+                        self.check_stack_underflow(2);
+                        self.stack[self.sp - 2] = self.stack[self.sp - 2] / self.stack[self.sp - 1];
                         self.sp -= 1;
                         self.ip += 1;
                     }
@@ -113,25 +223,41 @@ mod test {
 
     #[test]
     fn single_push() {
-        let program = vec![Inst::Push(10)];
+        let program = vec![Inst::Push(10.into())];
 
         let svm = setup_and_run(program);
-        assert_eq!(svm.stack[0], 10);
+        assert_eq!(svm.stack[0], 10.into());
     }
 
     #[test]
     fn single_add() {
-        let program = vec![Inst::Push(10), Inst::Push(15), Inst::Add];
+        let program = vec![Inst::Push(10.into()), Inst::Push(15.into()), Inst::Add];
 
         let svm = setup_and_run(program);
-        assert_eq!(svm.stack[0], 25);
+        assert_eq!(svm.stack[0], 25.into());
     }
 
     #[test]
     fn single_sub() {
-        let program = vec![Inst::Push(15), Inst::Push(10), Inst::Sub];
+        let program = vec![Inst::Push(15.into()), Inst::Push(10.into()), Inst::Sub];
 
         let svm = setup_and_run(program);
-        assert_eq!(svm.stack[0], 5);
+        assert_eq!(svm.stack[0], 5.into());
+    }
+
+    #[test]
+    fn single_mul() {
+        let program = vec![Inst::Push(2.into()), Inst::Push(10.into()), Inst::Mul];
+
+        let svm = setup_and_run(program);
+        assert_eq!(svm.stack[0], 20.into());
+    }
+
+    #[test]
+    fn single_div() {
+        let program = vec![Inst::Push(15.into()), Inst::Push(3.into()), Inst::Div];
+
+        let svm = setup_and_run(program);
+        assert_eq!(svm.stack[0], 5.into());
     }
 }
