3123c1c728e970fdbe9d286a60a14cb3b30357d4
jan <defPelly@proton.me>
2025-07-13 16:19:53 +0200

    Reorganize project structure and introduce examples folder

---
+++ examples/fib.rs
@@ -0,0 +1,29 @@
+use stacked::svm::*;
+
+fn main() {
+    let fib_n: Program = vec![
+        Inst::Push(0.into()),
+        Inst::Push(1.into()),
+        Inst::Push(0.into()),
+        Inst::Push(8.into()),
+        Inst::Write,
+        Inst::Push(0.into()),
+        Inst::Loadi,
+        Inst::Push(0.into()),
+        Inst::Lte,
+        Inst::Jump1(20),
+        Inst::Push(0.into()),
+        Inst::Push(0.into()),
+        Inst::Loadi,
+        Inst::Push(1.into()),
+        Inst::Sub,
+        Inst::Write,
+        Inst::Dupe(1),
+        Inst::Dupe(1),
+        Inst::Add,
+        Inst::Jump(5),
+        Inst::Print,
+    ];
+    let mut svm = Svm::from_program(fib_n);
+    svm.run();
+}
